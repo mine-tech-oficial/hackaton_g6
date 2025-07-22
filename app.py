1c2c29ec05ccb4f28582f0672200e9cfeda06e42
Kev1n269 <knrbadaro@gmail.com>
2025-07-22 17:34:55 -0300

    adicionar sistema de formulário

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+import csv 
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.get('/add_work')
+def index():
+    return  render_template('add work.html')
+
+@app.post('/add_work')
+def add_new_work():
+    id=1
+    with open("works.csv", mode="r", encoding="utf-8") as csv_file:
+        reader=list(csv.reader(csv_file))
+        if len(reader) > 1:
+            last_row = reader[-1]
+        if last_row:
+            id += int(last_row[0])
+    
+    work_name=request.form.get('work_name')
+
+    original_work_date=request.form.get('work_date')
+    work_date= datetime.strptime(original_work_date, "%Y-%m-%d")
+
+    work_date = work_date.strftime("%m/%d/%Y")
+
+    work_decription=request.form.get('workwork_description')
+
+    new_work=[
+        id,
+        work_date,
+        work_decription,
+
+    ]
+
+    with open("works.csv", mode="a", encoding="utf-8") as csv_file:
+        writer=csv.writer(csv_file)
+        writer.writerow(new_work)
+
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
